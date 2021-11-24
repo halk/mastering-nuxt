44d9b3c6c2b1812261836ec95a6719717478a3ed
Halil Köklü <halil@koklu.me>
2021-11-24 22:25:08 +0300

    some config for eslint and spell checking

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'airbnb-base',
+    'plugin:import/recommended',
+    '@nuxtjs',
+    'plugin:vue/essential',
+    'plugin:nuxt/recommended',
+    'prettier',
+  ],
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  plugins: [
+    'vue',
+  ],
+  rules: {
+    'vue/multi-word-component-names': 'off',
+    'import/extensions': [
+      'error',
+      'always',
+      {
+        js: 'never',
+        vue: 'never',
+      },
+    ],
+  },
+  settings: {
+    'import/extensions': ['.js', '.vue'],
+    'import/resolver': {
+      'eslint-import-resolver-custom-alias': {
+        alias: {
+          '~': './'
+        },
+        extensions: ['.js', '.vue']
+      }
+    }
+  }
+};
