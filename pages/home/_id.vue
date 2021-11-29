833a5724622a333b5d0ef73dfff11dc8fc6868a7
Halil Köklü <halil@koklu.me>
2021-11-29 10:18:46 +0300

    How Does Routing in Nuxt Work?

---
+++ pages/home/_id.vue
@@ -0,0 +1,18 @@
+<template>
+<div>{{ home.title }}</div>
+</template>
+
+<script>
+import homes from '~/data/homes.json';
+
+export default {
+  data() {
+    return {
+      home: {}
+    }
+  },
+  created() {
+    this.home = homes.find((home) => home.objectID === this.$route.params.id);
+  }
+};
+</script>
