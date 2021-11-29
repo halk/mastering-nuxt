b43dca5bdab66b84b2b52ff525703096c072ae8d
Halil Köklü <halil@koklu.me>
2021-11-29 10:25:31 +0300

    Creating the Property Page

--- pages/home/_id.vue
+++ pages/home/_id.vue
@@ -1,5 +1,14 @@
 <template>
-<div>{{ home.title }}</div>
+<div>
+  <div style="display: flex;">
+    <img v-for="image in home.images" :key="image" :src="image" width="200" height="150"/>
+  </div>
+  {{ home.title }}<br/>
+  ${{ home.pricePerNight }} / night<br/>
+  <img src="/images/marker.svg" width="20" height="20"/>{{ home.location.address }} {{ home.location.city }} {{ home.location.state }} {{ home.location.country }}<br />
+  <img src="/images/star.svg" width="20" height="20"/>{{ home.reviewValue }}<br/>
+  {{ home.guests }} guests, {{ home.bedrooms }} rooms, {{ home.beds }} beds, {{ home.bathrooms }} baths<br/>
+</div>
 </template>
 
 <script>
@@ -10,6 +19,11 @@ export default {
     return {
       home: {}
     }
+  },
+  head() {
+    return {
+      title: this.home.title
+    };
   },
   created() {
     this.home = homes.find((home) => home.objectID === this.$route.params.id);
